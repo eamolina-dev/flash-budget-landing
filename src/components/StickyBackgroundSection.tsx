b0d3d0348a6c38d9b4143b2239be1b1c748bc97c
eamolina-dev <eamolina.dev@gmail.com>
2026-04-16 05:19:46 -0300

    Remove unused components and dead starter CSS

--- src/components/StickyBackgroundSection.tsx
+++
@@ -1,9 +0,0 @@
-export const StickyBackgroundSection = ({ image }: { image: string }) => {
-  return (
-    <section className="relative h-[160vh]">
-      <div className="sticky top-0 h-screen">
-        <img src={image} className="w-full h-full object-cover" />
-      </div>
-    </section>
-  );
-};
--- src/components/StickyImageSection.tsx
+++
@@ -1,9 +0,0 @@
-export const StickyImageSection = ({ image }: { image: string }) => {
-  return (
-    <section className="relative h-[220vh]">
-      <div className="sticky top-0 h-screen w-full">
-        <img src={image} className="w-full h-full object-cover" />
-      </div>
-    </section>
-  );
-};
